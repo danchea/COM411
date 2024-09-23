f8e94c4814175169101e9cda3a4e7aa634c618d6
Daniel Cheah <2chead33@solent.ac.uk>
2024-09-23 14:50:04 +0100

    added code to display ASCII art robot

--- Week 1/Output_task.py
+++ Week 1/Output_task.py
@@ -1,3 +1,10 @@
+print("welcome to COM411")
+print("In week 1 we will learn...")
+print()
+print("...how to use Github")
+print("...How to output to the screen")
+print("...How to get user input")
+
 print("\n Displays a new line")
 print("\t Displays a tab space")
 print("\\ Displays a back slash")
@@ -5,3 +12,11 @@ print("\" Displays a double quote")
 print("\' Displays a single quote")
 
 print("\"I am programming!\"")
+
+print("##########")
+print("# 0    0 #")
+print("#  ----  #")
+print("##########")
+
+
+
