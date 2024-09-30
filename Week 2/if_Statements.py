e6e69b78b0f05551962331a3bb71b2b477166460
Daniel Cheah <2chead33@solent.ac.uk>
2024-09-30 14:36:57 +0100

    task 4

--- Week 2/if_Statements.py
+++ Week 2/if_Statements.py
@@ -1,4 +1,4 @@
-##Task 1
+# #Task 1
 #
 # book = input("enter type of book")
 # if book == "adventure":
@@ -13,19 +13,26 @@
 # else:
 #     print("performing activity")
 # print("Activity completed!")
+#
+# #Task 3
+#
+# direction = input("enter a direction (up, left, down, right)")
+# holder = "down"
+# if direction == "up":
+#     holder = "upwards"
+# elif direction == "left":
+#     holder = "leftwards"
+# elif direction == "right":
+#     holder = "rightwards"
+# else:
+#     print("Invalid direction")
+#     exit()
+#
+# print (f"i am moving {holder}")
 
-##Task 3
-
-direction = input("enter a direction (up, left, down, right)")
-holder = "down"
-if direction == "up":
-    holder = "upwards"
-elif direction == "left":
-    holder = "leftwards"
-elif direction == "right":
-    holder = "rightwards"
+##Task 4
+number =  int(input("Enter a whole number: "))
+if number % 2 == 0:
+    print("Even")
 else:
-    print("Invalid direction")
-    exit()
-
-print (f"i am moving {holder}")
+    print("Odd")
