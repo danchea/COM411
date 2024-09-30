68fe802f94a18d0ffd2b0e51d0a8714eea490369
Daniel Cheah <2chead33@solent.ac.uk>
2024-09-30 13:54:43 +0100

    Week 2

---
+++ Week 2/main.py
@@ -0,0 +1,16 @@
+age = int(input("Enter your age: "))
+print(f"your age is {age}")
+
+if age >= 16:
+    print("you can play thr lottery")
+else:
+    print("you cannot play the lottery")  ##if and else and elif
+
+location = input("Enter your location: ").lower()
+
+if location == "Southampton":
+    print("set alarm for 8:30")
+elif location == "IOW":
+    print("set alarm for 7:30")
+else:
+    print(" set alarm for 6:30")
