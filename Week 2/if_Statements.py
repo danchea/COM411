c79fbb9417ece98ce1459dc4e15873d5920d3023
Daniel Cheah <2chead33@solent.ac.uk>
2024-09-30 14:20:55 +0100

    Task 2

--- Week 2/if_Statements.py
+++ Week 2/if_Statements.py
@@ -1,4 +1,15 @@
+##Task 1
+
 book = input("enter type of book")
 if book == "adventure":
     print("i like adventure books!")
-print("finish reading book!")
+print("finish reading book!")  ##outside of if loop to print no matter the input
+
+##Task 2
+
+activity = input("enter an activity")
+if activity == "calculate":
+    print("performing calculations")
+else:
+    print("performing activity")
+print("Activity completed!")
