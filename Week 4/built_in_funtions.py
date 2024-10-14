741c18b5e9d4bea11f0a2b327c91fead57bbd4b2
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-14 14:34:42 +0100

    added code to demonstrate abs, range,chr

--- Week 4/built_in_funtions.py
+++ Week 4/built_in_funtions.py
@@ -1,4 +1,15 @@
-char = (input("Program Started!\nEnter a character: "))
-num = len(char)
-if num == 1:
-    print("The ASCII code for t is:",ord(char),"\nProgram Ended!")
+## Task 1
+#
+# char = (input("Program Started!\nEnter a character: "))
+# num = len(char)
+# if num == 1:
+#     print("The ASCII code for t is:",ord(char),"\nProgram Ended!")
+
+## Task2
+
+code = input("Program Started\nPlease enter an ASCII code")
+num = abs(ord(code))
+if num <= 126 and num >= 32:
+    print(f"the character represented by the ASCII code {code} is {num} \nprogram Ended!")
+else:
+    print("error, incorrect input")
