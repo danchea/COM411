c9d60d1e48951a5d82b6f22e28c1ab5911919b49
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-14 14:48:35 +0100

    added code to demonstrate the use of a user-defined function with a nested loop

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -1,4 +1,15 @@
-def listen():
-    sound = input("What sound did you hear?\n")
-    print(f"that was a loud {sound}!")
-listen()
+## Task 3
+#
+# def listen():
+#     sound = input("What sound did you hear?\n")
+#     print(f"that was a loud {sound}!")
+# listen()
+
+## Task 4
+def identify():
+    view = input("what lies before us?")
+    if view == "a large boulder":
+        print("its time to run")
+    else:
+        print("we'll be fine")
+identify()
