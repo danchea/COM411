a7e0d358f4cd1cb46ff2e3a27be4298bc0d6367a
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-14 14:42:58 +0100

    added code to demonstrate the use of a user-defined function.

--- Week 4/built_in_funtions.py
+++ Week 4/built_in_funtions.py
@@ -7,9 +7,12 @@
 
 ## Task2
 
-code = input("Program Started\nPlease enter an ASCII code")
-num = abs(ord(code))
-if num <= 126 and num >= 32:
-    print(f"the character represented by the ASCII code {code} is {num} \nprogram Ended!")
-else:
-    print("error, incorrect input")
+# code = input("Program Started\nPlease enter an ASCII code")
+# num = abs(ord(code))
+# if num <= 126 and num >= 32:
+#     print(f"the character represented by the ASCII code {code} is {num} \nprogram Ended!")
+# else:
+#     print("error, incorrect input")
+
+## Task 3
+
---
+++ Week 4/custom_functions.py
@@ -0,0 +1,4 @@
+def listen():
+    sound = input("What sound did you hear?\n")
+    print(f"that was a loud {sound}!")
+listen()
