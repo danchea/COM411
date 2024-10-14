2503478e553d92e9da20becb646fcf04aa2cbec0
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-14 14:59:10 +0100

    added code to demonstrate the use of user-defined function with a parameter.

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -6,10 +6,25 @@
 # listen()
 
 ## Task 4
-def identify():
-    view = input("what lies before us?")
-    if view == "a large boulder":
-        print("its time to run")
+# def identify():
+#     view = input("what lies before us?")
+#     if view == "a large boulder":
+#         print("its time to run")
+#     else:
+#         print("we'll be fine")
+# identify()
+
+## Task 5
+
+def escape_by(plan):
+    if plan == "jumping over":
+        print("we cannot escape that way! the boulder is too big!")
+    elif plan == "running around":
+        print("we cannot escape that way! the boulder is moving too fast!")
+    elif plan == "cross bridge ahead":
+        print("that might just work! lets go!")
     else:
-        print("we'll be fine")
-identify()
+        print("we cannot escape that way! the boulder is in the way")
+escape_by("jumping over")
+escape_by("running around")
+escape_by("cross bridge ahead")
