e889f27c84b5e28000e0d14f94d0d58a4a29e948
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-14 15:20:02 +0100

    added code to demonstrate the use of a user defined function with a loop

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -16,15 +16,29 @@
 
 ## Task 5
 
-def escape_by(plan):
-    if plan == "jumping over":
-        print("we cannot escape that way! the boulder is too big!")
-    elif plan == "running around":
-        print("we cannot escape that way! the boulder is moving too fast!")
-    elif plan == "cross bridge ahead":
-        print("that might just work! lets go!")
+# def escape_by(plan):
+#     if plan == "jumping over":
+#         print("we cannot escape that way! the boulder is too big!")
+#     elif plan == "running around":
+#         print("we cannot escape that way! the boulder is moving too fast!")
+#     elif plan == "cross bridge ahead":
+#         print("that might just work! lets go!")
+#     else:
+#         print("we cannot escape that way! the boulder is in the way")
+# escape_by("jumping over")
+# escape_by("running around")
+# escape_by("cross bridge ahead")
+
+## Task 6
+
+def cross_bridge (steps):
+    if steps <= 5 and steps >= 0:
+        for i in range(steps):
+            print("Crossed Step")
+        print("We must keep going!")
     else:
-        print("we cannot escape that way! the boulder is in the way")
-escape_by("jumping over")
-escape_by("running around")
-escape_by("cross bridge ahead")
+        for i in range(steps):
+            print("Crossed Step")
+        print("the bridge is collapsing")
+cross_bridge(3)
+cross_bridge(6)
