09b965478aa2e02ef4b196816eb2d9eee15dc13f
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-21 14:35:31 +0100

    added code to demonstrate how to iterate a list

--- week 5/list_tasks.py
+++ week 5/list_tasks.py
@@ -6,7 +6,7 @@
 #     steps.append("Move Backward")
 #     steps.append("Turn Left")
 #     steps.append("Turn Right")
-#     print(steps)
+#     return steps
 #
 # def run_task1():
 #     directions()
@@ -15,18 +15,33 @@
 # run_task1()
 
 ## Task 2
+#
+# def movements():
+#     path = ["forward", 10, "backward", 5 , "left", 3, "right",1]
+#     return path
+#
+# def run_task2():
+#     print("moving...")
+#     directions = movements()
+#     print(f"move",directions[0],"for",directions[1],"steps")
+#     print(f"move", directions[2], "for", directions[3], "steps")
+#     print(f"move", directions[4], "for", directions[5], "steps")
+#     print(f"move", directions[6], "for", directions[7], "steps")
+#
+# run_task2()
 
-def movements():
-    path = ["forward", 10, "backward", 5 , "left", 3, "right",1]
-    return path
+## Task 3
 
-def run_task2():
-    print("moving...")
-    directions = movements()
-    print(f"move",directions[0],"for",directions[1],"steps")
-    print(f"move", directions[2], "for", directions[3], "steps")
-    print(f"move", directions[4], "for", directions[5], "steps")
-    print(f"move", directions[6], "for", directions[7], "steps")
+def directions():
+    steps = ["move forward", "move backward", "turn left", "turn right"]
+    return steps
 
-run_task2()
+def menu():
+    print("please select a direction")
+    direction = directions()
+    count = 0
+    for d in direction:
+        print(count,":",d)
+        count += 1
 
+menu()
