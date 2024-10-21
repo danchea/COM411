bdf61e2ca5a86428cbd54fce29758a35d0eb9bc9
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-21 14:19:33 +0100

    Added code to demonstrate how to index a list

--- week 5/list_tasks.py
+++ week 5/list_tasks.py
@@ -1,18 +1,32 @@
-##Task 1
-
-def directions():
-    steps = []
-    steps.append("Move Forward")
-    steps.append("Move Backward")
-    steps.append("Turn Left")
-    steps.append("Turn Right")
-    print(steps)
-
-def run_task1():
-    directions()
-
-
-run_task1()
+# ##Task 1
+#
+# def directions():
+#     steps = []
+#     steps.append("Move Forward")
+#     steps.append("Move Backward")
+#     steps.append("Turn Left")
+#     steps.append("Turn Right")
+#     print(steps)
+#
+# def run_task1():
+#     directions()
 
 
+# run_task1()
+
+## Task 2
+
+def movements():
+    path = ["forward", 10, "backward", 5 , "left", 3, "right",1]
+    return path
+
+def run_task2():
+    print("moving...")
+    directions = movements()
+    print(f"move",directions[0],"for",directions[1],"steps")
+    print(f"move", directions[2], "for", directions[3], "steps")
+    print(f"move", directions[4], "for", directions[5], "steps")
+    print(f"move", directions[6], "for", directions[7], "steps")
+
+run_task2()
 
