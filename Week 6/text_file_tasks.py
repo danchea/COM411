8ab691f94abd4cf1dfd3d004c9e04bfc7f9b3aed
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-28 14:26:58 +0000

    added code to display info about the current working directory

--- Week 6/text_file_tasks.py
+++ Week 6/text_file_tasks.py
@@ -1,1 +1,8 @@
 def cwd():
+    import os
+    path = os.getcwd()
+    print(f"the current working directory is:\n{path}")
+    for file in os.listdir(path):
+        print("The directory contains the following files:\n", file)
+
+cwd()
