4afd76a2d3701d27d214f27aeef0d2a0613a50d6
Daniel Cheah <2chead33@solent.ac.uk>
2024-10-28 15:11:32 +0000

    added code to read a text file line by line

--- Week 6/text_file_tasks.py
+++ Week 6/text_file_tasks.py
@@ -15,32 +15,37 @@
 
 ## Task 2
 
-def display_chars(file_path,no_chars):
-    with open(file_path) as f:
-        print(f.read(no_chars),"\n")
+# def display_chars(file_path,no_chars):
+#     with open(file_path) as f:
+#         print(f.read(no_chars),"\n")
+#
+# def display_lines(file_path):
+#     with open(file_path) as f:
+#         print(f.readline())
+#
+# def display_text (file_path):
+#     with open(file_path) as f:
+#         print(f.read())
+#
+# def run_task2 ():
+#     display_chars("library.txt", 5)
+#     display_lines("library.txt")
+#     display_text("library.txt")
+#
+# if __name__ == '__main__':
+#     run_task2()
+#
 
-def display_lines(file_path):
-    with open(file_path) as f:
-        print(f.readline())
+## Task 3
 
-def display_text (file_path):
-    with open(file_path) as f:
-        print(f.read())
+def search(file_name):
+    print("Searching...")
+    with open(file_name) as f:
+        for line in f:
+            print("looked in",line.strip())
+        print("...Done")
 
-def run_task2 ():
-    display_chars("library.txt", 5)
-    display_lines("library.txt")
-    display_text("library.txt")
+def run_task3():
+    search("library.txt")
 
-if __name__ == '__main__':
-    run_task2()
-
-
-
-
-
-
-
-
-
-
+run_task3()
