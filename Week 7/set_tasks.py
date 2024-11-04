f198c118936ee3a9672163b692cdbce47f2608e5
Daniel Cheah <2chead33@solent.ac.uk>
2024-11-04 14:22:08 +0000

    coded solution to demonstrate a simple set

---
+++ Week 7/set_tasks.py
@@ -0,0 +1,10 @@
+##Task 1
+
+# def observed():
+#     observations = {"car", "Sky Scraper", "Sky Scraper", "Bike", "House", "House"}
+#     return observations
+#
+# def run_task1():
+#     print(observed())
+#
+# run_task1()
